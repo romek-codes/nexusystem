e727dc6eab4f0955c059652e4b2daa96ba288ea8
romek <contact@romek.codes>
2025-08-09 22:36:20 +0200

    update contributing.md, remove hadi's ssh key

--- server-modules/ssh.nix
+++ server-modules/ssh.nix
@@ -11,8 +11,6 @@
   };
 
   users.users."${config.var.username}" = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPG9SE80ZyBcXZK/f5ypSKudaM5Jo3XtQikCnGo0jI5E hadi@nixy"
-    ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 ..." ];
   };
 }
