55bb59951a430a0cd5e5e00147274c8f3ad82ad8
romek <contact@romek.codes>
2026-03-14 19:10:46 +0100

    feat(gaming): add prismlauncher
    Include Prism Launcher when gaming packages are enabled.

--- home/gaming.nix
+++ home/gaming.nix
@@ -1,9 +1,16 @@
-{ pkgs, config, lib, ... }: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
   config = lib.mkIf (config.var.withGames or false) {
     home.packages = with pkgs; [
       (lutris.override { extraPkgs = pkgs: [ fuse ]; })
       pcsx2
       rpcs3
+      prismlauncher
       # linux-wallpaperengine # wallpaper engine for linux
       # godot_4 # Gamedev
     ]; # ++ lib.optionals (config.var.gpu.type == "amd") [ # amdvlk # deprecated ];
