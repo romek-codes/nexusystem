4da7bc486f27ba2ef62fdbae8161ea4223ba0382
romek <contact@romek.codes>
2026-04-25 20:51:00 +0200

    fix(docker): disable boot-time dockerd startup

--- nixos/docker.nix
+++ nixos/docker.nix
@@ -1,4 +1,8 @@
 { config, ... }: {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    # Start dockerd on first use via docker.socket instead of blocking boot.
+    enableOnBoot = false;
+  };
   users.users."${config.var.username}".extraGroups = [ "docker" ];
 }
