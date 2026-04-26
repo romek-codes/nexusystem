58b59db21e999ffc3b910405e60b423161534086
romek <contact@romek.codes>
2026-04-26 18:39:02 +0200

    fix(glance): override page background via custom css

--- nixos/glance.nix
+++ nixos/glance.nix
@@ -1,7 +1,13 @@
 # Dashboard page. Accessible via http://localhost:2048
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
-  rgb-to-hsl = color:
+  rgb-to-hsl =
+    color:
     let
       r = ((lib.toInt config.lib.stylix.colors."${color}-rgb-r") * 100.0) / 255;
       g = ((lib.toInt config.lib.stylix.colors."${color}-rgb-g") * 100.0) / 255;
@@ -10,24 +16,33 @@ let
       min = lib.min r (lib.min g b);
       delta = max - min;
       fmod = base: int: base - (int * builtins.floor (base / int));
-      h = if delta == 0 then
-        0
-      else if max == r then
-        60 * (fmod ((g - b) / delta) 6)
-      else if max == g then
-        60 * (((b - r) / delta) + 2)
-      else if max == b then
-        60 * (((r - g) / delta) + 4)
-      else
-        0;
+      h =
+        if delta == 0 then
+          0
+        else if max == r then
+          60 * (fmod ((g - b) / delta) 6)
+        else if max == g then
+          60 * (((b - r) / delta) + 2)
+        else if max == b then
+          60 * (((r - g) / delta) + 4)
+        else
+          0;
       l = (max + min) / 2;
-      s = if delta == 0 then
-        0
-      else
-        100 * delta / (100 - lib.max (2 * l - 100) (100 - (2 * l)));
+      s = if delta == 0 then 0 else 100 * delta / (100 - lib.max (2 * l - 100) (100 - (2 * l)));
       roundToString = value: toString (builtins.floor (value + 0.5));
-    in lib.concatMapStringsSep " " roundToString [ h s l ];
-in {
+    in
+    lib.concatMapStringsSep " " roundToString [
+      h
+      s
+      l
+    ];
+  customCss = pkgs.writeText "glance-custom.css" ''
+    :root {
+      --color-background: #${config.lib.stylix.colors.base00} !important;
+    }
+  '';
+in
+{
   services = {
     glance = {
       enable = true;
@@ -37,228 +52,239 @@ in {
           primary-color = lib.mkForce (rgb-to-hsl "base05");
           negative-color = lib.mkForce "358 100 68";
           contrast-multiplier = lib.mkForce 1.2;
+          custom-css-file = "/assets/glance-custom.css";
         };
-        pages = [{
-          hide-desktop-navigation = true;
-          columns = [
-            {
-              size = "small";
-              widgets = [
-                {
-                  type = "bookmarks";
-                  groups = [
-                    {
-                      title = "";
-                      same-tab = true;
-                      color = "200 50 50";
-                      links = [
-                        {
-                          title = "Github";
-                          url = "https://github.com/romek-codes/";
-                        }
-                        {
-                          title = "Tutanota";
-                          url = "https://tutanota.com/";
-                        }
-                        {
-                          title = "Coolify";
-                          url = "https://coolify.romek.codes/";
-                        }
-                        {
-                          title = "Hetzner";
-                          url = "https://console.hetzner.com/projects";
-                        }
-                        {
-                          title = "Decodo";
-                          url =
-                            "https://dashboard.decodo.com/residential-proxies/statistics";
-                        }
-                        {
-                          title = "PostHog";
-                          url = "https://posthog.com";
-                        }
-                        {
-                          title = "WhatsApp";
-                          url = "https://web.whatsapp.com/";
-                        }
-                      ];
-                    }
-                    {
-                      title = "Docs";
-                      same-tab = true;
-                      color = "200 50 50";
-                      links = [
-                        {
-                          title = "Nixpkgs repo";
-                          url = "https://github.com/NixOS/nixpkgs";
-                        }
-                        {
-                          title = "Nixvim";
-                          url = "https://nix-community.github.io/nixvim/";
-                        }
-                        {
-                          title = "Hyprland wiki";
-                          url = "https://wiki.hyprland.org/";
-                        }
-                        {
-                          title = "Search NixOS";
-                          url = "https://search.nixos.org/packages";
-                        }
-                      ];
-                    }
-                    {
-                      title = "Helpful";
-                      same-tab = true;
-                      color = "200 50 50";
-                      links = [
-                        {
-                          title = "Svgl";
-                          url = "https://svgl.app";
-                        }
-                        {
-                          title = "Cobalt (Downloader)";
-                          url = "https://cobalt.tools";
-                        }
-                        {
-                          title = "Mazanoke (Image optimizer)";
-                          url = "https://mazanoke.com";
-                        }
-                      ];
-                    }
-                  ];
-                }
-                {
-                  type = "rss";
-                  limit = 10;
-                  collapse-after = 3;
-                  cache = "12h";
-                  feeds = [
-                    {
-                      url = "https://selfh.st/rss/";
-                      title = "selfh.st";
-                      limit = 4;
-                    }
-                    { url = "https://ciechanow.ski/atom.xml"; }
-                    {
-                      url = "https://www.joshwcomeau.com/rss.xml";
-                      title = "Josh Comeau";
-                    }
-                    { url = "https://samwho.dev/rss.xml"; }
-                    {
-                      url = "https://ishadeed.com/feed.xml";
-                      title = "Ahmad Shadeed";
-                    }
-                  ];
-                }
-                {
-                  type = "twitch-channels";
-                  channels = [ "theprimeagen" ];
-                }
-                {
-                  type = "releases";
-                  cache = "1d";
-                  repositories = [
-                    # "NixOS/nixpkgs"
-                    "hyprwm/hyprland"
-                    "immich-app/immich"
-                    "syncthing/syncthing"
-                    "Aider-AI/aider"
-                    "neovim/neovim"
-                  ];
-                }
-              ];
-            }
-            {
-              size = "full";
-              widgets = [
-                {
-                  type = "group";
-                  widgets = [{ type = "hacker-news"; }];
-                }
-                {
-                  type = "videos";
-                  channels = [
-                    "UCR-DXc1voovS8nhAvccRZhg" # Jeff Geerling
-                    "UCsBjURrPoezykLs9EqgamOA" # Fireship
-                    "UCHnyfMqiRRG1u-2MsSQLbXA" # Veritasium
-                  ];
-                }
-                {
-                  type = "group";
-                  widgets = [
-                    {
-                      type = "reddit";
-                      subreddit = "technology";
-                      show-thumbnails = true;
-                    }
-                    {
-                      type = "reddit";
-                      subreddit = "selfhosted";
-                      show-thumbnails = true;
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              size = "small";
-              widgets = [
-                {
-                  type = "clock";
-                  hour-format = "24h";
-                }
-                {
-                  type = "weather";
-                  location = "Recklinghausen, Germany";
-                }
-                {
-                  type = "markets";
-                  markets = [
-                    {
-                      symbol = "SPY";
-                      name = "S&P 500";
-                    }
-                    {
-                      symbol = "FTSE All World";
-                      name = "VWCE.DE";
-                    }
-                    {
-                      symbol = "BTC-USD";
-                      name = "Bitcoin";
-                    }
-                    {
-                      symbol = "NVDA";
-                      name = "NVIDIA";
-                    }
-                    {
-                      symbol = "AAPL";
-                      name = "Apple";
-                    }
-                    {
-                      symbol = "MSFT";
-                      name = "Microsoft";
-                    }
-                    {
-                      symbol = "GOOGL";
-                      name = "Google";
-                    }
-                    {
-                      symbol = "AMD";
-                      name = "AMD";
-                    }
-                    {
-                      symbol = "AMZN";
-                      name = "AMAZON";
-                    }
-                  ];
-                }
-              ];
-            }
-          ];
-          name = "Home";
-        }];
-        server = { port = 2048; };
+        pages = [
+          {
+            hide-desktop-navigation = true;
+            columns = [
+              {
+                size = "small";
+                widgets = [
+                  {
+                    type = "bookmarks";
+                    groups = [
+                      {
+                        title = "";
+                        same-tab = true;
+                        color = "200 50 50";
+                        links = [
+                          {
+                            title = "Github";
+                            url = "https://github.com/romek-codes/";
+                          }
+                          {
+                            title = "Tutanota";
+                            url = "https://tutanota.com/";
+                          }
+                          {
+                            title = "Coolify";
+                            url = "https://coolify.romek.codes/";
+                          }
+                          {
+                            title = "Hetzner";
+                            url = "https://console.hetzner.com/projects";
+                          }
+                          {
+                            title = "Decodo";
+                            url = "https://dashboard.decodo.com/residential-proxies/statistics";
+                          }
+                          {
+                            title = "PostHog";
+                            url = "https://posthog.com";
+                          }
+                          {
+                            title = "WhatsApp";
+                            url = "https://web.whatsapp.com/";
+                          }
+                        ];
+                      }
+                      {
+                        title = "Docs";
+                        same-tab = true;
+                        color = "200 50 50";
+                        links = [
+                          {
+                            title = "Nixpkgs repo";
+                            url = "https://github.com/NixOS/nixpkgs";
+                          }
+                          {
+                            title = "Nixvim";
+                            url = "https://nix-community.github.io/nixvim/";
+                          }
+                          {
+                            title = "Hyprland wiki";
+                            url = "https://wiki.hyprland.org/";
+                          }
+                          {
+                            title = "Search NixOS";
+                            url = "https://search.nixos.org/packages";
+                          }
+                        ];
+                      }
+                      {
+                        title = "Helpful";
+                        same-tab = true;
+                        color = "200 50 50";
+                        links = [
+                          {
+                            title = "Svgl";
+                            url = "https://svgl.app";
+                          }
+                          {
+                            title = "Cobalt (Downloader)";
+                            url = "https://cobalt.tools";
+                          }
+                          {
+                            title = "Mazanoke (Image optimizer)";
+                            url = "https://mazanoke.com";
+                          }
+                        ];
+                      }
+                    ];
+                  }
+                  {
+                    type = "rss";
+                    limit = 10;
+                    collapse-after = 3;
+                    cache = "12h";
+                    feeds = [
+                      {
+                        url = "https://selfh.st/rss/";
+                        title = "selfh.st";
+                        limit = 4;
+                      }
+                      { url = "https://ciechanow.ski/atom.xml"; }
+                      {
+                        url = "https://www.joshwcomeau.com/rss.xml";
+                        title = "Josh Comeau";
+                      }
+                      { url = "https://samwho.dev/rss.xml"; }
+                      {
+                        url = "https://ishadeed.com/feed.xml";
+                        title = "Ahmad Shadeed";
+                      }
+                    ];
+                  }
+                  {
+                    type = "twitch-channels";
+                    channels = [ "theprimeagen" ];
+                  }
+                  {
+                    type = "releases";
+                    cache = "1d";
+                    repositories = [
+                      # "NixOS/nixpkgs"
+                      "hyprwm/hyprland"
+                      "immich-app/immich"
+                      "syncthing/syncthing"
+                      "Aider-AI/aider"
+                      "neovim/neovim"
+                    ];
+                  }
+                ];
+              }
+              {
+                size = "full";
+                widgets = [
+                  {
+                    type = "group";
+                    widgets = [ { type = "hacker-news"; } ];
+                  }
+                  {
+                    type = "videos";
+                    channels = [
+                      "UCR-DXc1voovS8nhAvccRZhg" # Jeff Geerling
+                      "UCsBjURrPoezykLs9EqgamOA" # Fireship
+                      "UCHnyfMqiRRG1u-2MsSQLbXA" # Veritasium
+                    ];
+                  }
+                  {
+                    type = "group";
+                    widgets = [
+                      {
+                        type = "reddit";
+                        subreddit = "technology";
+                        show-thumbnails = true;
+                      }
+                      {
+                        type = "reddit";
+                        subreddit = "selfhosted";
+                        show-thumbnails = true;
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                size = "small";
+                widgets = [
+                  {
+                    type = "clock";
+                    hour-format = "24h";
+                  }
+                  {
+                    type = "weather";
+                    location = "Recklinghausen, Germany";
+                  }
+                  {
+                    type = "markets";
+                    markets = [
+                      {
+                        symbol = "SPY";
+                        name = "S&P 500";
+                      }
+                      {
+                        symbol = "FTSE All World";
+                        name = "VWCE.DE";
+                      }
+                      {
+                        symbol = "BTC-USD";
+                        name = "Bitcoin";
+                      }
+                      {
+                        symbol = "NVDA";
+                        name = "NVIDIA";
+                      }
+                      {
+                        symbol = "AAPL";
+                        name = "Apple";
+                      }
+                      {
+                        symbol = "MSFT";
+                        name = "Microsoft";
+                      }
+                      {
+                        symbol = "GOOGL";
+                        name = "Google";
+                      }
+                      {
+                        symbol = "AMD";
+                        name = "AMD";
+                      }
+                      {
+                        symbol = "AMZN";
+                        name = "AMAZON";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+            name = "Home";
+          }
+        ];
+        server = {
+          port = 2048;
+          assets-path = "/var/lib/glance/assets";
+        };
       };
     };
   };
+
+  systemd.services.glance.preStart = ''
+    mkdir -p /var/lib/glance/assets
+    cp ${customCss} /var/lib/glance/assets/glance-custom.css
+    chmod 0644 /var/lib/glance/assets/glance-custom.css
+  '';
 }
