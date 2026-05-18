27f68d03f8f85a27d9a5a3337afb7f8d40010ba0
romek <contact@romek.codes>
2026-05-18 19:05:31 +0200

    fix(systemd): reset blueman applet ExecStart override

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,8 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   environment.systemPackages = with pkgs; [ blueman ];
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
   services.blueman.enable = true;
+
+  systemd.user.services."blueman-applet" = {
+    overrideStrategy = "asDropin";
+    unitConfig = {
+      PartOf = [ "graphical-session.target" ];
+    };
+    serviceConfig = {
+      ExecStart = lib.mkForce [
+        ""
+        "${pkgs.blueman}/bin/blueman-applet"
+      ];
+      Restart = "on-failure";
+    };
+    wantedBy = [ "graphical-session.target" ];
+  };
 }
